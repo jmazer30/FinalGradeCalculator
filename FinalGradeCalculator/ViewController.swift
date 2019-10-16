7e54c4bcf861bfcf7a3e22be174c4d5dd9707f8c
jmazer30 <jmazer27@gmail.com>
2019-10-16 11:24:07 -0400

    Initial Commit

---
+++ FinalGradeCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FinalGradeCalculator
+//
+//  Created by  on 10/16/19.
+//  Copyright © 2019 JordansEpicApps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
